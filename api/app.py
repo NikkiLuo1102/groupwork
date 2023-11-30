a6a359443821801ed9bc3e1d894590f8eeb71fed
nl1023 <nl1023@imperial.ac.uk>
2023-11-30 22:46:34 +0000

    first

--- api/app.py
+++ api/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, jsonify
 import psycopg2 as db
-import hashlib
+# import hashlib
 
 # #创建哈希
 # def hash_password(password):
@@ -8,50 +8,57 @@ import hashlib
 #     return hashed_password
 
 #验证哈希
-def verify_password(input_password, hashed_password):
-    input_hashed_password = hashlib.sha256(input_password.encode()).hexdigest()
-    return input_hashed_password == hashed_password
+# def verify_password(input_password, hashed_password):
+#     input_hashed_password = hashlib.sha256(input_password.encode()).hexdigest()
+#     return input_hashed_password == hashed_password
 
 app = Flask(__name__)
+
+# 连接 MySQL 数据库
+def get_db_connection():
+    server_params = {
+        'dbname': 'nl1023',
+        'host': 'db.doc.ic.ac.uk',
+        'port': '5432',
+        'user': 'nl1023',
+        'password': 'aFZK-3CzFH*j3y',
+        'client_encoding': 'utf-8'
+    }
+    return db.connect(**server_params)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
-    # return "Hello , my new app!"
 
 
 @app.route("/login")
 def login():
     return render_template("login.html")
 
-# 连接 MySQL 数据库
-server_params = {
-    'dbname': 'mondial',
-    'host': 'db.doc.ic.ac.uk',
-    'port': '5432',
-    'user': 'nl1023',
-    'password': 'lab',
-    'client_encoding': 'utf-8'
-}
-
-conn = db.connect(**server_params)
-
-# 创建游标对象
-cursor = db.cursor()
 
 # 处理登录请求
 @app.route("/login", methods=["POST"])
 def submit():
-    username = request.form.get("username")
-    password = request.form.get("keyword")
+    data = request.json  # 获取 JSON 数据
+
+    # 获取用户名和密码
+    username = data.get("username")
+    password = data.get("password")
+
+    # 连接数据库
+    conn = get_db_connection()
+
+    # 创建游标对象
+    cursor = conn.cursor()
 
     # 查询数据库中是否存在该用户
-    query = "SELECT * FROM users WHERE username = %s"
+    query = "SELECT * FROM my_user WHERE name = %s"
     cursor.execute(query, (username,))
     user = cursor.fetchone()
-
-    if user and verify_password(password,user[2]):  
+    print(password,user[6])
+    if user and password==user[6]:  
         return jsonify({'message': 'Login successful'})
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
+
 
